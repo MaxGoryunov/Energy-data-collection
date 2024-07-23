ddba9616e3c4ffb20a24a000ca7224a4c915a54b
Max Goryunov <maximthehedgehog@yandex.ru>
2024-07-24 02:49:18 +0300

    get capital names in English

--- daylight_hours.py
+++ daylight_hours.py
@@ -8,17 +8,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
-wikipedia.set_lang("ru")
-
-
 def region_capitals():
-    url = "https://ru.wikipedia.org/wiki/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B5_%D1%86%D0%B5%D0%BD%D1%82%D1%80%D1%8B_%D1%81%D1%83%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2_%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%B9_%D0%A4%D0%B5%D0%B4%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8"
+    url = "https://en.wikipedia.org/wiki/Federal_subjects_of_Russia"
     page = requests.get(url)
     print(page.status_code)
     soup = BeautifulSoup(page.text, 'html.parser')
-    table = soup.find_all("table", class_=["standard", "sortable", "jquery-tablesorter"])
-    df = pd.concat(pd.read_html(StringIO(str(table))))["Административный центр"]
+    table = soup.find_all("table", class_=["wikitable", "sortable", "jquery-tablesorter"])
+    df = pd.concat(pd.read_html(StringIO(str(table[2]))))
+    df: pd.DataFrame = df.iloc[:, df.columns.get_level_values(1) == "Capital/ Administrative centre[a]"]
+    df = (
+        df.replace("\[d\]", "", regex=True)
+        .replace("^.* \(de facto\) ", "", regex=True)
+        .replace(" \(claimed\)", "", regex=True)
+        .replace(" \(Largest.*$", "", regex=True)
+    )
+    df.columns = ["Capital"]
+    print(df)
+    # df = df["Административный центр"].dropna().reset_index(drop=True)
     return df
 
 
-
+def capitals_coordinates(capitals: pd.DataFrame):
+    for index, capital in capitals.items():
+        page = wikipedia.page(capital)
+        soup = BeautifulSoup(page.html(), "html.parser")
+        coordinates = soup.find("span", class_="geo-inline").text
+        print(page.title, coordinates)
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from functools import reduce
 import pandas as pd
 from population import subject_names, yearly_population, test_non_found_regions, format_search_data, remaining_regions, \
     fill_remaining_yearly_population
-from daylight_hours import region_capitals
+from daylight_hours import region_capitals, capitals_coordinates
 
 
 if __name__ == '__main__':
     caps = region_capitals()
-    print(caps)
+    print(type(caps))
+    # capitals_coordinates(caps)
