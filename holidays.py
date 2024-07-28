983c8f8d2e58088347917b726a0581e701362406
Max Goryunov <maximthehedgehog@yandex.ru>
2024-07-29 02:06:59 +0300

    add holiday track script for all years

--- holidays.py
+++ holidays.py
@@ -1,15 +1,48 @@
 import requests
 import pandas as pd
 import numpy as np
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+from population import subject_names
 
-def get_month_tables(year: int):
+
+def month_tables(year: int):
     url = f"https://www.consultant.ru/law/ref/calendar/proizvodstvennye/{year}/"
     page = requests.get(url)
     soup = BeautifulSoup(page.text, "html.parser")
-    months = soup.find_all("table", class_="cal")
+    return soup.find_all("table", class_="cal")
+
+
+def process_months(months: list[Tag], year: int):
+    order = 1
+    holidays = []
     for month in months:
         name = month.find("th", class_="month")
-        days = month.find_all("td", class_=["holiday", "weekend", "work"])
+        days = month.select("td:not(.inactively)")
+        for day in days:
+            dd = int(day.text.replace("*", ""))
+            date = ""
+            if dd < 10:
+                date += "0"
+            date += f"{dd}."
+            if order < 10:
+                date += "0"
+            date += f"{order}.{year}"
+            if "weekend" in day["class"] or "holiday" in day["class"]:
+                holidays.append([date, 1])
+            else:
+                holidays.append([date, 0])
+            # print(day.text.strip(), day["class"])
+        order += 1
+        # print(name.text)
+    return pd.DataFrame.from_records(np.array(holidays))
 
-        print(name.text, len(days))
+
+def process_years():
+    dfs = []
+    for year in range(2012, 2024):
+        months = month_tables(year)
+        df = process_months(months, year)
+        dfs.append(df)
+        print(year, len(df))
+    blueprint = pd.concat(dfs, ignore_index=True)
+    print(blueprint)
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from functools import reduce
 
 import pandas as pd
 
-from holidays import get_month_tables
+from holidays import month_tables, process_months, process_years
 from population import subject_names, yearly_population, test_non_found_regions, format_search_data, remaining_regions, \
     fill_remaining_yearly_population
 from daylight_hours import region_capitals, capitals_coordinates
 
 
 if __name__ == '__main__':
-    get_month_tables(2012)
+    process_years()
